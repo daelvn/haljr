8cc0e7ad8e8ec0fcae1764cb68731d325d6579be
Dael Muñiz <daelvn@gmail.com>
2022-05-31 18:07:29 +0000

    Modify pull.js to not rebase

--- pull.js
+++ pull.js
@@ -1,5 +1,15 @@
 const { exec } = require('child_process');
 
+exec("git config pull.rebase false", (err, stdout, stderr) => {
+    if (err) {
+        console.error(`exec error: ${err}`);
+        return;
+    }
+    console.log(`${stdout}`);
+});
+
+const { exec } = require('child_process');
+
 exec("git pull origin main", (err, stdout, stderr) => {
     if (err) {
         console.error(`exec error: ${err}`);
