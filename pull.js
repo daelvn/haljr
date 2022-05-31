6596c953bf82cf7baf1179514c933b9c9fd7ee26
Dael Muñiz <daelvn@gmail.com>
2022-05-31 18:09:22 +0000

    Force pull always

--- pull.js
+++ pull.js
@@ -8,9 +8,7 @@ exec("git config pull.rebase false", (err, stdout, stderr) => {
     console.log(`${stdout}`);
 });
 
-const { exec } = require('child_process');
-
-exec("git pull origin main", (err, stdout, stderr) => {
+exec("git pull origin main --force", (err, stdout, stderr) => {
     if (err) {
         console.error(`exec error: ${err}`);
         return;
