fbbb6db75bb040778f64fa0fb8d888c8065c6c2f
Dael Muñiz <daelvn@gmail.com>
2022-06-04 20:21:00 +0000

    Push all kin commands

---
+++ commands/kin-add.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, Permissions } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kin-add')
+        .setDescription('Gives yourself a kin')
+        .addStringOption(option =>
+            option.setName('name')
+            .setDescription('Name of the character you kin.')
+            .setRequired(true)),
+    async execute(interaction) {
+        const roleName = interaction.options.getString('name');
+        const currentUser = interaction.guild.client.user;
+
+        // resolve role
+        console.log(`Searching for ${roleName.toLowerCase() + " in the flesh"}`)
+        const role = interaction.guild.roles.cache.find(role => role.name === roleName.toLowerCase() + " in the flesh")
+        if (role == null) {
+            return interaction.reply("Could not find a role for that kin!");
+        }
+
+        // check that role is within bounds
+        if ((role.position > interaction.guild.roles.resolve('982715881287188501').position) && (role.position < interaction.guild.roles.botRoleFor(currentUser).position - 1)) {
+            interaction.member.roles.add(role);
+
+            // reply
+            const replyEmbed = new MessageEmbed()
+                .setColor(role.hexColor)
+                .setTitle("Assigned a kin!")
+                .setDescription(`We gave you the kin ${roleName}`);
+            return interaction.reply({ embeds: [replyEmbed] })
+        } else {
+            return interaction.reply(`You cannot give yourself that kin!`)
+        }
+    },
+};
---
+++ commands/kin-request.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, Permissions } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kin-request')
+        .setDescription('Requests the creation of a kin role to the YYHY admins')
+        .addStringOption(option =>
+            option.setName('name')
+            .setDescription('Name of the character you kin.')
+            .setRequired(true))
+        .addStringOption(option =>
+            option.setName('hex')
+            .setDescription('HEX color for the requested role.')
+            .setRequired(true)),
+    async execute(interaction) {
+        const roleName = interaction.options.getString('name').replace(/ ?in the flesh/gi, "");
+        const roleColor = interaction.options.getString('hex');
+
+        // create role request
+        try {
+            const RoleRequests = interaction.client.models.get('RoleRequests');
+            const roleRequest = await RoleRequests.create({
+                name: roleName + " in the flesh",
+                hex: roleColor,
+                username: interaction.user.id
+            });
+
+            // reply
+            const replyEmbed = new MessageEmbed()
+                .setColor(`#${roleRequest.hex}`)
+                .setTitle("Your kin role has been requested!")
+                .setDescription(`Requested kin role for ${roleName} with color #${roleRequest.hex}`);
+            await interaction.reply({ embeds: [replyEmbed] });
+        } catch (error) {
+            await interaction.reply(`Something went wrong with the role request. ${error.name}: ${error.message}`);
+        }
+    },
+};
