b590704fa6109ede38d774159d80808297c78ab5
Dael Muñiz <daelvn@gmail.com>
2022-06-13 18:19:46 +0000

    Format and update intents

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
 // Node libraries
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require("node:fs");
+const path = require("node:path");
 
 // Sequelize
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 // Dates
-const dayjs = require('dayjs');
+const dayjs = require("dayjs");
 
 // Node scheduler
-const schedule = require('node-schedule');
+const schedule = require("node-schedule");
 
 // Require the necessary discord.js classes
-const { Client, Intents, Collection } = require('discord.js');
-const { token, guildId } = require('./config.json');
+const { Client, Intents, Collection } = require("discord.js");
+const { token, guildId } = require("./config.json");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
+});
 
 // Connect a database
-const sql = new Sequelize('database', 'user', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
-    logging: false,
-    // SQLite only
-    storage: 'yyhy.sqlite',
+const sql = new Sequelize("database", "user", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  // SQLite only
+  storage: "yyhy.sqlite",
 });
 
 // Instantiate all models
 client.models = new Collection();
-const modelsPath = path.join(__dirname, 'models');
-const modelFiles = fs.readdirSync(modelsPath).filter(file => file.endsWith('.js'));
+const modelsPath = path.join(__dirname, "models");
+const modelFiles = fs
+  .readdirSync(modelsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of modelFiles) {
-    const filePath = path.join(modelsPath, file);
-    const model = require(filePath);
+  const filePath = path.join(modelsPath, file);
+  const model = require(filePath);
 
-    let sqldef = sql.define(model.data.name.toLowerCase(), model.data.schema);
-    console.log(`Adding model: ${model.data.name} -> ${typeof sqldef} (${typeof sql})`);
-    client.models.set(model.data.name, sqldef);
-    console.log(`Set: ${model.data.name} = ${client.models.get(model.data.name)}`);
+  let sqldef = sql.define(model.data.name.toLowerCase(), model.data.schema);
+  console.log(
+    `Adding model: ${model.data.name} -> ${typeof sqldef} (${typeof sql})`
+  );
+  client.models.set(model.data.name, sqldef);
+  console.log(
+    `Set: ${model.data.name} = ${client.models.get(model.data.name)}`
+  );
 }
 
 // Globalize scheduler
@@ -48,29 +56,33 @@ client.scheduled = new Collection();
 
 // Add a commands collection
 client.commands = new Collection();
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    // Set a new item in the Collection
-    // With the key as the command name and the value as the exported module
-    client.commands.set(command.data.name, command);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
 }
 
 // Load events
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs
+  .readdirSync(eventsPath)
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-    const filePath = path.join(eventsPath, file);
-    const event = require(filePath);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
-    }
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
 }
 
 // Login to Discord with your client's token
