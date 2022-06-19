131396d43ebb2cb1aaeb6f6f2d90f26de7f17a0a
Dael Muñiz <daelvn@gmail.com>
2022-06-19 22:52:08 +0000

    Fixes `role-request` errors when using a Hex sign #2

--- commands/kin-request.js
+++ commands/kin-request.js
@@ -1,39 +1,33 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, Permissions } = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, Permissions } = require("discord.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('kin-request')
-        .setDescription('Requests the creation of a kin role to the YYHY admins')
-        .addStringOption(option =>
-            option.setName('name')
-            .setDescription('Name of the character you kin.')
-            .setRequired(true))
-        .addStringOption(option =>
-            option.setName('hex')
-            .setDescription('HEX color for the requested role.')
-            .setRequired(true)),
-    async execute(interaction) {
-        const roleName = interaction.options.getString('name').replace(/ ?in the flesh/gi, "");
-        const roleColor = interaction.options.getString('hex');
+  data: new SlashCommandBuilder()
+    .setName("kin-request")
+    .setDescription("Requests the creation of a kin role to the YYHY admins")
+    .addStringOption((option) => option.setName("name").setDescription("Name of the character you kin.").setRequired(true))
+    .addStringOption((option) => option.setName("hex").setDescription("HEX color for the requested role.").setRequired(true)),
+  async execute(interaction) {
+    const roleName = interaction.options.getString("name").replace(/ ?in the flesh/gi, "");
+    const roleColor = interaction.options.getString("hex").replace(/#/g, "");
 
-        // create role request
-        try {
-            const RoleRequests = interaction.client.models.get('RoleRequests');
-            const roleRequest = await RoleRequests.create({
-                name: roleName + " in the flesh",
-                hex: roleColor,
-                username: interaction.user.id
-            });
+    // create role request
+    try {
+      const RoleRequests = interaction.client.models.get("RoleRequests");
+      const roleRequest = await RoleRequests.create({
+        name: roleName + " in the flesh",
+        hex: roleColor,
+        username: interaction.user.id,
+      });
 
-            // reply
-            const replyEmbed = new MessageEmbed()
-                .setColor(`#${roleRequest.hex}`)
-                .setTitle("Your kin role has been requested!")
-                .setDescription(`Requested kin role for ${roleName} with color #${roleRequest.hex}`);
-            await interaction.reply({ embeds: [replyEmbed] });
-        } catch (error) {
-            await interaction.reply(`Something went wrong with the role request. ${error.name}: ${error.message}`);
-        }
-    },
+      // reply
+      const replyEmbed = new MessageEmbed()
+        .setColor(`#${roleRequest.hex}`)
+        .setTitle("Your kin role has been requested!")
+        .setDescription(`Requested kin role for ${roleName} with color #${roleRequest.hex}`);
+      await interaction.reply({ embeds: [replyEmbed] });
+    } catch (error) {
+      await interaction.reply(`Something went wrong with the role request. ${error.name}: ${error.message}`);
+    }
+  },
 };
