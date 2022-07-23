b68a613748c6d04cf209453d31e7c9948c9e0ee3
Dael Muñiz <daelvn@gmail.com>
2022-07-23 10:54:38 +0000

    Added pagination to role-list

--- commands/role-list.js
+++ commands/role-list.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed, Permissions } = require("discord.js");
 const { endOfRoles, roleRelativePosition } = require("../config.json");
+const { ButtonPaginator } = require("@psibean/discord.js-pagination");
 
 module.exports = {
   data: new SlashCommandBuilder().setName("role-list").setDescription("Lists all available roles"),
@@ -9,14 +10,26 @@ module.exports = {
     const finalRole = interaction.guild.roles.resolve(endOfRoles);
     const botRole = interaction.guild.roles.botRoleFor(currentUser);
 
+    await interaction.deferReply();
+
     interaction.guild.roles.fetch().then((roles) => {
+      var pages = [];
       let rolemap = roles
         .sort((a, b) => b.position - a.position)
         .map((r) => r)
         .filter((r) => r.position < botRole.position + roleRelativePosition) // list begin
         .filter((r) => r.position > finalRole.position) // list end
         .join("\n");
-      if (rolemap.length > 1024) rolemap = "Too many roles to display";
+      // split role list into several embeds of 1024 max length
+      if (rolemap.length > 1024) {
+        let parts = rolemap.match(/[\s\S]{1,920}/gm);
+        console.log(`Rolemap length (${rolemap.length}) vs. 920s (${parts.length})`);
+        for (let [n, part] of parts.entries()) {
+          pages.push(new MessageEmbed().addField(`Role List (page ${n + 1}/${parts.length})`, part));
+        }
+        const buttonPaginator = new ButtonPaginator(interaction, { pages });
+        return buttonPaginator.send();
+      }
       if (!rolemap) rolemap = "No roles";
 
       const replyEmbed = new MessageEmbed().addField("Role List", rolemap);
