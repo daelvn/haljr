fba8f8abb75b431b744b2da877a307fc78c67224
Dael Muñiz <daelvn@gmail.com>
2022-07-31 22:05:38 +0000

    deploy-commands now adapts to new config

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,25 +1,28 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+const fs = require("node:fs");
+const path = require("node:path");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { clientId, guilds, token } = require("./config.json");
 
 // Create collection of commands from folder
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath);
-    commands.push(command.data.toJSON());
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
 }
 
 // Register the commands
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: "9" }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(() => console.log('Successfully registered application commands.'))
+for (let { guildId, guilds } of servers.entries()) {
+  rest
+    .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log(`Successfully registered application commands for ${guild.name}.`))
     .catch(console.error);
+}
